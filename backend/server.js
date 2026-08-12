ce1274f2c8d708a71e8062a851c7e3ff5010e041
Aarav <coolaarav1008@gmail.com>
2026-08-12 12:16:12 -0400

    fix: mount bills API routes

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,4 @@
-const express = require('express');
+﻿const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
@@ -13,10 +13,14 @@ app.use(express.json());
 const transactionRoutes = require('./src/routes/transactionRoutes');
 const alertRoutes = require('./src/routes/alertRoutes');
 const subscriptionRoutes = require('./src/routes/subscriptionRoutes');
+const billRoutes = require('./src/routes/billRoutes');
+const billRoutes = require('./src/routes/billRoutes');
 
 app.use('/api/transactions', transactionRoutes);
 app.use('/api/alerts', alertRoutes);
 app.use('/api/subscriptions', subscriptionRoutes);
+app.use('/api/bills', billRoutes);
+app.use('/api/bills', billRoutes);
 
 app.get('/', (req, res) => {
   res.send('Transaction Intelligence API is running');
